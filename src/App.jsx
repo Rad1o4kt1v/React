e0d145a9bc0eaa27ae4b35496e58d5f0b5db9fd0
Rad1o4kt1v <abraevradmir2@gmail.com>
2025-08-28 12:50:59 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './components1/Header/Header.jsx';
+import Footer from './components1/Footer/Footer.jsx';
+import UserProfile from './components1/UserProfile/UserProfile.jsx';
+
+export default function App() {
+    return (
+        <>
+            <Header />
+            <UserProfile />
+            <Footer />
+        </>
+    );
+}
+
---
+++ src/components1/UserProfile/UserProfile.jsx
@@ -0,0 +1,19 @@
+import './UserProfile.css'
+
+function UserProfile () {
+    return (
+        <div>
+            <nav>
+                <ul className='profile-card'>
+                    <li>Radio</li>
+                    <li>abraevradmir2@gmail.com</li>
+                    <li>
+                        <img src="https://i.pinimg.com/736x/fb/7c/6b/fb7c6bc5083d3c6ae0e3da1cc68bf9e5.jpg"/>
+                    </li>
+                    </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default UserProfile
